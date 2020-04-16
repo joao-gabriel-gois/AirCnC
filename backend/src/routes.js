7e42844a2f07c9bf51ba8e9afbd08fe5e761172e
Joao Gabriel <thereis@noone.coml>
2020-04-16 03:21:45 -0300

    Adjusting Routes files

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,8 @@ const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
