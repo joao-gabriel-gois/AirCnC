675688417a10770d29198af7e358465108c0dec2
Joao Gabriel <thereis@noone.coml>
2020-04-16 03:19:31 -0300

    Adding approval and rejection routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -21,6 +21,10 @@ routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 routes.get('/dashboard', DashboardController.show);
 
-routes.post('/spots/:spot_id/bookings', BookingController.store)
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
+
 
 module.exports = routes;
